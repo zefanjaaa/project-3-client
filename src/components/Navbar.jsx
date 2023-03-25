32792e6da8f0fff8e440c769be02c46ceb923f98
Zefanja Timorason <z.timorason@gmail.com>
2023-03-25 09:48:28 +0100

    logout button working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
-
+import { AuthContext } from '../context/auth.context'
 function Navbar() {
+
+  const {isLoggedIn,user,logOutUser} = useContext(AuthContext)
   return (
-    <div>
+    <nav>
 
 <Link to="/"><button> Home</button></Link>
 <Link to="/auth/signup"><button> Create an account</button></Link>
-    <Link to="/auth/login"><button>Login</button></Link>
+      <Link to="/auth/login"><button>Login</button></Link>
+      <button onClick={logOutUser}> Log Out</button>
+      {/* {isLoggedIn && ( 
+        <>
+          <button onClick={logOutUser}> Log Out</button>
+          <p>{user && user.name}</p>
+        </>
+      )} */}
   
   
 
     
 
     
-    </div>
+    </nav>
   )
 }
 
