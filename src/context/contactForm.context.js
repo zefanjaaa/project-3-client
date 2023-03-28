a8641cf8a54bef9a5846a3362bc1a78241f06073
Zefanja Timorason <z.timorason@gmail.com>
2023-03-28 18:54:39 +0200

    started on the contact form context

---
+++ src/context/contactForm.context.js
@@ -0,0 +1,10 @@
+import React, {  useEffect } from 'react'
+
+const contactContext = React.createContext()
+
+function contactProviderWrapper() {
+    
+
+}
+
+export {contactContext,contactProviderWrapper}
