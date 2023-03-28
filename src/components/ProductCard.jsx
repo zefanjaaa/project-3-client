c828dd835060cb441510bcbce9c55001a73a4376
Alexandra <ss_sergeeva167@mail.ru>
2023-03-28 18:05:07 +0100

    hollo

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
+import {FaShoppingCart, FaBookmark, } from "react-icons/fa";
+
+
 
 function ProductCard({price, image, brand, nameOfProduct}) {
 
  return (
-    <div>
+  
+    <div className='prodList'>
+    <div key={nameOfProduct} className="prodCard">
 
-    <img src={image} alt="pic-product"/>
+    <img src={image} alt="pic-product" className='prodCardPic'/>
 
-    <p><b>Brand : </b>{brand}</p>
-    <p><b>Image : {nameOfProduct}</b></p>
-    <p><b>Price : {price}</b></p>
+    <FaShoppingCart className={"prodCardCard"} />
+    <FaBookmark className={"prodCardWish"}/>
     
+
+    <div className='prodCardContent'>
+
+    <h3 className='prodName'>{nameOfProduct}</h3>
+    <h3 className='prodPrice'>${price}</h3>
+    <h3 className='prodPrice'>{brand}</h3>
+  
+   </div>
+
+
+    </div>
+
     </div>
   )
 }
 
 export default ProductCard
+
+// <div className='displayStack'></div>
+//          <div className='prodPrice'>${price}</div>
+        
+//    </div>
