e3f115d5ca46fc5163fe3f75c9650a1a07a4a899
Alexandra <ss_sergeeva167@mail.ru>
2023-04-06 18:29:56 +0100

    changes

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,85 +1,74 @@
-import React, { useContext } from 'react'
-import { useState } from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import { AuthContext } from '../context/auth.context'
-import '../style/Login.css'
+import React, { useContext } from "react";
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { AuthContext } from "../context/auth.context";
+import "../style/Login.css";
 import * as RiIcons from "react-icons/ri";
 import * as MdIcons from "react-icons/md";
 
 const API = "http://localhost:5005";
 
 function LoginPage() {
+  const [err, setErr] = useState(undefined);
 
-const [err, setErr] = useState(undefined) 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-const [email, setEmail] = useState('')
-const [password, setPassword] = useState('')
-const navigate = useNavigate()
+  const { storeToken, authenticateUser } = useContext(AuthContext);
 
-const {storeToken, authenticateUser} = useContext(AuthContext)
+  const handleEmail = (event) => setEmail(event.target.value);
+  const handlePassword = (event) => setPassword(event.target.value);
 
-const handleEmail = (event) => setEmail(event.target.value)
-const handlePassword = (event) => setPassword(event.target.value)
-
-
-function handleSubmit(e){
-   e.preventDefault()
-   const body = {email, password}
-   axios.post(`${API}/auth/login`, body)
-       .then((response) => {
-           storeToken(response.data.authToken)
-           authenticateUser()
-   navigate('/wishlist')
-  
-      
-   })
-   .catch((error)=>{
-    console.log(error)
-   const description = error.response.data.message
-   setErr(description)
-      
-     })
-   }
-   
-
+  function handleSubmit(e) {
+    e.preventDefault();
+    const body = { email, password };
+    axios
+      .post(`${API}/auth/login`, body)
+      .then((response) => {
+        storeToken(response.data.authToken);
+        authenticateUser();
+        navigate("/wishlist");
+      })
+      .catch((error) => {
+        console.log(error);
+        const description = error.response.data.message;
+        setErr(description);
+      });
+  }
 
   return (
-  <div className='all'>
-  <div className="login">
-  
-  <h2>Login</h2>
-  <h3>Welcome Back</h3>
- 
-  <form  className='login-form' maction="" onSubmit={handleSubmit}>
-  <div className='textbox'>
-  <input type="email" value={email} onChange={handleEmail}/>
-  <span htmlFor="">
-   
-   <MdIcons.MdOutlineAlternateEmail></MdIcons.MdOutlineAlternateEmail>
- 
- </span>
- </div>
+    <div className="all">
+      <div className="login">
+        <h2>Login</h2>
+        <h3>Welcome Back</h3>
 
- <div className='textbox'>
- <input type="password" value={password} onChange={handlePassword}/>
-  <span htmlFor="">
+        <form className="login-form" maction="" onSubmit={handleSubmit}>
+          <div className="textbox">
+            <input type="email" value={email} onChange={handleEmail} />
+            <span htmlFor="">
+              <MdIcons.MdOutlineAlternateEmail></MdIcons.MdOutlineAlternateEmail>
+            </span>
+          </div>
 
-  <RiIcons.RiLockPasswordLine></RiIcons.RiLockPasswordLine>
- 
- </span>
- </div>
- <button type='submit'>LOGIN</button>
- 
- 
-    
- {err && <p>{err}</p>}
+          <div className="textbox">
+            <input type="password" value={password} onChange={handlePassword} />
+            <span htmlFor="">
+              <RiIcons.RiLockPasswordLine></RiIcons.RiLockPasswordLine>
+            </span>
+          </div>
+          <button type="submit">LOGIN</button>
 
- <NavLink className="NavLinK" to={"/auth/signup"}>Don't have an account? Sign in</NavLink>
- </form>
- </div>
- </div>
-  )
+          {err && <p>{err}</p>}
+
+          <NavLink className="NavLinK" to={"/auth/signup"}>
+            Don't have an account? Sign in
+          </NavLink>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
