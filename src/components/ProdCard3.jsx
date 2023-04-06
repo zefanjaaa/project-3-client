83d842bdf68fe73e1f9eaa96c270dc110bda3993
Alexandra <ss_sergeeva167@mail.ru>
2023-04-06 14:16:52 +0100

    fixing card

--- src/components/ProdCard3.jsx
+++ src/components/ProdCard3.jsx
@@ -1,42 +1,38 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import "../style/ProdCard3.css"
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "../style/ProdCard3.css";
 // import { color, motion } from "framer-motion";
-import React from 'react'
+import React from "react";
 import { CartContex } from "../context/cartContex";
 import { useContext } from "react";
 import { Col, Form, Row } from "react-bootstrap";
 import * as FaIcons from "react-icons/fa";
 import * as BsIcons from "react-icons/bs";
-import { Link } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-function ProdCard3({ price, image, brand, nameOfProduct,_id }) {
+function ProdCard3({ price, image, brand, nameOfProduct, _id }) {
+  const API_URL = "http://localhost:3000";
 
-   const API_URL = "http://localhost:3000"
-  
-   
+  return (
+    <Card border="light" style={{ width: "15rem" }} key={nameOfProduct} className="CardAll">
+      <Card.Img className="pic" variant="top" src={image} alt="pic-product" />
 
-
-return (
-
-    <Card style={{ width: '15rem'}}  key={nameOfProduct}>
-      <Card.Img className='pic' whileHover={{ scale: 0.9 }} variant="top" src={image} alt="pic-product"/>
-
-      <Card.Body className='cardBody'>
-        <Card.Title className='BrandText'>{brand}</Card.Title>
+      <Card.Body className="cardBody">
         <Card.Text>
-        <Link to={`${API_URL}/product/${_id}`}>
-        <h3 className="prodNameText">{nameOfProduct}</h3>
-        </Link>
-        <Card.Text className="prodPriceText">${price}</Card.Text>
+          <Card.Text className="ProdName">
+            <NavLink className="active" to={`${API_URL}/product/${_id}`}>
+              <p>{nameOfProduct}</p>
+            </NavLink>
+          </Card.Text>
 
-        <FaIcons.FaHeart></FaIcons.FaHeart>
+          <Card.Text>
+            <p className="prodBrandText">{brand}</p>
+          </Card.Text>
 
+          <p className="prodPriceText">
+            ${price} <FaIcons.FaHeart></FaIcons.FaHeart>
+          </p>
         </Card.Text>
-
-        
-
-        
       </Card.Body>
     </Card>
   );
