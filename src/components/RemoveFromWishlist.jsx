0aa208af3a12eaf1dcfad18b1dd032041a88e861
Zefanja Timorason <z.timorason@gmail.com>
2023-04-10 23:00:50 +0200

    should work now

--- src/components/RemoveFromWishlist.jsx
+++ src/components/RemoveFromWishlist.jsx
@@ -1,38 +1,24 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import axios from "axios";
 
 import { AuthContext } from "../context/auth.context";
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
 
-function RemoveFromWishlist({ productId }) {
-    const [removeData, setRemoveData] = useState([]);
-    const [wishlist, setWishlist] = useState([])
-
+function RemoveFromWishlist({productId,getWishlist}) {
+ 
   const { user } = useContext(AuthContext);
 
-  useEffect(() => {
-    const findToken = localStorage.getItem("authToken");
 
-    axios
-      .get(`${API_URL}/auth/verify`, {
-        headers: { Authorization: `Bearer ${findToken}` },
-      })
 
-      .then((response) => {
-        const data = response.data.wishlist;
-        setRemoveData(data);
-      })
-        .catch((error) => {
-          console.log(`there is an error getting the wishlist data`,error)
-      })
-  }, []);
-
-  const remove = (productId) => {
-      axios.delete(`product/${user._id}/wishlist/${productId}`)
+    const remove = () => {
+        const token = localStorage.getItem('authToken')
+        
+      axios.delete(`${API_URL}/product/${user._id}/wishlist/${productId}`,{headers: {Authorization:`Bearer ${token}`}} )
           .then((response) => {
-              console.log(response.data)
-              setWishlist(wishlist.filter((item) => item !== productId))
+        
+              getWishlist()
+              alert('The product is removed from your wishlist!')
           })
           .catch((error) => {
           console.log(`there is an error remove the product from wishlist`,error)
