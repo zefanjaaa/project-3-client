7e2ce7481c167491b3ae227d7199c27a4da8de49
Zefanja Timorason <z.timorason@gmail.com>
2023-04-11 15:51:38 +0200

    contact thing second try

--- src/pages/WishlistUserPage.jsx
+++ src/pages/WishlistUserPage.jsx
@@ -3,11 +3,11 @@ import { AuthContext } from "../context/auth.context";
 import EditUser from "../components/EditUser";
 import ContactFormResult from "../components/ContactFormResult";
 import RenderWishlist from "../components/RenderWishlist";
-
+import { useParams } from "react-router-dom";
 
 function WishlistUserPage() {
   const { user, administrator } = useContext(AuthContext);
-
+const {contactId} = useParams()
   const [edit, setEdit] = useState(false);
   const [contact, setContact] = useState(false);
   const [wishlist,setWishlist] = useState(false)
@@ -36,11 +36,11 @@ function WishlistUserPage() {
       {administrator ? (
         <button onClick={handleContact}>Contact</button>
       ) : (<p></p>)}
-      {administrator && contact ? <ContactFormResult /> : <p></p>}
+      {administrator && contact ? <ContactFormResult contactId={contactId} /> : <p></p>}
 
 
       <button onClick={handleWishlist}>wishlist</button>
-{wishlist ?  <RenderWishlist /> : <p></p> }
+{wishlist ?  <RenderWishlist  /> : <p></p> }
     </div>
   );
 }
