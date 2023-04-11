e43df48eab63e56a99bd8b9af28dabad6171ac6d
Zefanja Timorason <z.timorason@gmail.com>
2023-04-11 15:48:39 +0200

    contact thing

--- src/components/ContactFormResult.jsx
+++ src/components/ContactFormResult.jsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import {Link }from 'react-router-dom'
+import {Link,useParams }from 'react-router-dom'
 import axios from 'axios'
 
 const API_URL = process.env.REACT_APP_API_URL||"http://localhost:5005";
 
 
-const API = "http://localhost:3000"
+const API = process.env.API2 || "http://localhost:3000"
 
 
 function ContactFormResult() {
 
     const [contact, setContact] = useState([])
-    
+    const {contactId} = useParams()
+
+
     const getAllContacts = () => {
         const storedToken = localStorage.getItem("authToken")
 
@@ -21,9 +23,26 @@ function ContactFormResult() {
 
             .then((response) => {
                 setContact(response.data)
-                console.log('CONTACT', response.data)
+                
+              
             })
         .catch((error) => console.log("THERE IS AN ERROR GETTING THE CONTACT",error))
+    }
+    console.log('contact', contact)
+
+
+    const removeContact = () => {
+        const token = localStorage.getItem('authToken')
+        
+        axios.delete(`${API_URL}/contacts/${contactId}`, { headers: { Authorization: `Bearer ${token}` } })
+       
+            .then(() => {
+                alert('Contact removed')
+                getAllContacts()
+            })
+            .catch((error) => {
+            console.log('there is an error deleting a contact!', error)
+        })
     }
 
     useEffect(() => {
@@ -35,11 +54,12 @@ function ContactFormResult() {
           <div key={contact._id}>
               <h1>Contact form result</h1>
               {contact.map((contact) => (
-                  <div>
+                  <div key={contact._id}>
                       <Link to={`${API}/contact/${contact._id}`}>
                           <p><b>Title:</b> {contact.title}</p>
                           </Link>
                       <p><b>Name:</b>{contact.name} </p>
+                      <button onClick={removeContact}>remove the contact</button>
                       </div>
               ))}
               </div>
